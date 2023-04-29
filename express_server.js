61d36ec96e27ee9aa33ab0ffc638866dac57dd12
mfried95 <m.friedman1995@gmail.com>
2023-04-29 12:47:22 -0400

    Added seprate stylesheet

--- express_server.js
+++ express_server.js
@@ -4,6 +4,8 @@ const app = express();
 const PORT = 8080;
 const bcrypt = require("bcryptjs");
 
+app.use(express.static("public"));
+
 // Import data objects
 const { users, urlDatabase } = require("./data");
 
